a51f060520c637081cc4be2e24a8042254a1ef55
elliot <elliotzen256@gmail.com>
2025-07-13 01:25:14 +0800

    nvim lsp key mapping;

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,7 +4,9 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>ca", "<cmd> lua vim.lsp.buf.code_action() <cr>")
+map("n", "<leader>fl", "<cmd> lua require 'telescope.builtin'.lsp_document_symbols{} <cr>")
 
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
